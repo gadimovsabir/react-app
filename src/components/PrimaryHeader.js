b169b44212cc0f8f18f793d3d317081e534192d2
Sabir <gadimovsabir@gmail.com>
2020-09-15 18:09:32 +0400

    Create a new component SearchInput

--- src/components/PrimaryHeader.js
+++ src/components/PrimaryHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import SearchInput from './SearchInput.js'
 
 class PrimaryHeader extends React.Component {
   render() {
     return (
       <div className="primary-header">
-        <div className="container">
-          <a href="/" className="logo"></a>
+        <div className="container d-flex justify-content-between align-items-center">
+          <span className="logo"></span>
+          <SearchInput />
         </div>
       </div>
     );
---
+++ src/components/SearchInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SearchInput extends React.Component {
+  render() {
+    return <input type="search" placeholder="Поиск" className="search-input" />;
+  }
+}
+
+export default SearchInput;
