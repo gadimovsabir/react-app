7ccc6a94aaa8efcef5d6862231d82a718e3936b4
Sabir <gadimovsabir@gmail.com>
2020-09-24 15:03:40 +0400

    Modified MainContent component

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,41 +6,42 @@ class MainContent extends React.Component {
     super(props);
     this.state = {
       items: [],
+      error: null,
     };
   }
 
   componentDidMount() {
     fetch('https://my-json-server.typicode.com/gadimovsabir/react-app/news')
       .then(response => response.json())
-      .then(result => {
-        this.setState({
-          items: result,
-        })
-      });
+      .then(
+        result => this.setState({items: result}),
+        error => this.setState({error})
+      );
   }
 
   render() {
-    const news = [];
-    news.push(this.state.items.map(
-      item => {
+    let data;
+
+    if (this.state.error) {
+      data = this.state.error;
+    } else {
+      data = this.state.items.map(item => {
         return (
-          <div className="col-xl-4 card-container" key={item.id}>
-            <NewsCard 
+          <div className="col-lg-4 card-container" key={item.key}>
+            <NewsCard
               title={item.title}
               imgSrc={item.imgSrc}
               published={item.published}
             />
           </div>
-        )
-      }
-    ));
+        );
+      }); 
+    }
 
     return (
       <main>
         <div className="container">
-          <div className="row">
-            {news}
-          </div>
+          <div className="row">{data}</div>
         </div>
       </main>
     );
